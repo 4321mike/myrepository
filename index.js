b74ff8dddd686143289ca7b124456f6209530601
mike <michealobogo@gmail.com>
2024-01-17 17:06:10 -0800

    basic controller

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world!")
+const express = require ("express")
+const server = express();
+
+server.get("/", function (req, res) {
+    res.send ("<h1>hello world</h1>");
+});
+
+server.listen(3000,function () {
+    console.log("server listening on port 3000");
+ });
